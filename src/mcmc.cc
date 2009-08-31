1c02b062e9b42e6fe2189e05c651f40c843f9891
Ingo Fründ <ingo.fruend@googlemail.com>
2009-08-31 17:22:48 +0200

    [NF] added MH and Hybrid MCMC with common base class

--- src/mcmc.cc
+++ src/mcmc.cc
@@ -1,13 +1,33 @@
 #include "mcmc.h"
 
-PsiMetropolisHastings::PsiMetropolisHastings ( const PsiPsychometric * pmf, const PsiData * dat, const PsiRandom * proposal )
-	: model(*pmf), data(*dat), currenttheta(pmf->getNparams(),0), newtheta(pmf->getNparams(),0), propose(proposal), stepwidths(pmf->getNparams,1),currentmomentum(pmf->getNparams()),newmomentum(pmf->getNparams()), qold(1)
-{}
+#ifdef DEBUG_MCMC
+#include <iostream>
+#include <iomanip>
+#endif
 
+/**********************************************************************
+ *
+ * MetropolisHastings sampling
+ *
+ */
 
-std::vector<double> PsiMetropolisHastings::sample ( void ) {
-	double qnew, lratio, acc(log(drand48()));
-	int prm, Nprm(model.getNparams);
+MetropolisHastings::MetropolisHastings ( const PsiPsychometric * pmf, const PsiData * dat, PsiRandom * proposal )
+	: PsiSampler ( pmf, dat ),
+	currenttheta(pmf->getNparams(),0),
+	newtheta(pmf->getNparams(),0),
+	propose(proposal),
+	stepwidths(pmf->getNparams(),.1),
+	qold(1),
+	accept(0)
+{
+	setTheta ( currenttheta );
+}
+
+std::vector<double> MetropolisHastings::draw ( void ) {
+	double qnew, lratio, acc(drand48());
+	const PsiPsychometric * model (getModel());
+	const PsiData * data (getData());
+	int prm, Nprm(model->getNparams());
 
 	// propose a new point
 	for (prm=0; prm<Nprm; prm++) {
@@ -15,15 +35,215 @@ std::vector<double> PsiMetropolisHastings::sample ( void ) {
 	}
 
 	// negative log posterior of the point
-	qnew = model.neglpost ( newtheta, &data );
-	lratio = qold - qnew;
+	qnew = model->neglpost ( newtheta, data );
+	lratio = exp(qold - qnew);
+	if (lratio>1) lratio = 1;
 
-	if (acc<ratio) {
+	if (acc<lratio) {
 		// accept the new point
 		qold = qnew;
 		currenttheta = newtheta;
-		currentdeviance = model.deviance ( currenttheta, &data );
+		currentdeviance = model->deviance ( currenttheta, data );
+		accept ++;
+#ifdef DEBUG_MCMC
+		std::cerr << " * ";
+#endif
 	}
+#ifdef DEBUG_MCMC
+	else
+		std::cerr << "   ";
+
+	std::cerr
+		<< "Q_old: " << std::setiosflags ( std::ios::fixed ) << qold
+		<< " Q_new: " << std::setiosflags ( std::ios::fixed ) << qnew
+		<< " P(accept):" << std::setiosflags ( std::ios::fixed ) << exp(qold-qnew);
+
+	std::cout << currenttheta[0] << "\n";
+#endif
 
 	return currenttheta;
+}
+
+void MetropolisHastings::setTheta ( const std::vector<double>& prm ) {
+	if (prm.size()==currenttheta.size())
+		currenttheta = prm;
+	else
+		throw BadArgumentError();
+	qold = getModel()->neglpost( currenttheta, getData() );
+}
+
+void MetropolisHastings::setstepsize ( double size, unsigned int param ) {
+	if ( param<getModel()->getNparams() )
+		stepwidths[param] = size;
+	else
+		throw BadIndexError();
+}
+
+void MetropolisHastings::setstepsize ( const std::vector<double>& sizes ) {
+	int i;
+	for (i=0; i<stepwidths.size(); i++)
+		stepwidths[i] = sizes[i];
+}
+
+PsiMClist MetropolisHastings::sample ( unsigned int N ) {
+	const PsiPsychometric * model ( getModel() );
+	PsiMClist out ( N, model->getNparams() );
+	int i;
+
+	for (i=0; i<N; i++) {
+		out.setEst ( i, draw(), 0. );
+		out.setdeviance ( i, getDeviance() );
+#ifdef DEBUG_MCMC
+		std::cerr << " accept: " << std::setiosflags ( std::ios::fixed ) << double(accept)/(i+1) << "\n";
+#endif
+	}
+
+#ifdef DEBUG_MCMC
+	std::cerr << "Acceptance rate: " << double(accept)/N << "\n";
+#endif
+
+	return out;
+}
+
+/**********************************************************************
+ *
+ * Hybird MCMC
+ *
+ */
+
+HybridMCMC::HybridMCMC ( const PsiPsychometric* Model, const PsiData* Data, int Nleap )
+	: PsiSampler ( Model, Data ),
+	currenttheta ( Model->getStart( Data ) ),
+	newtheta     ( Model->getNparams(), 0 ),
+	momentum  ( Model->getNparams(), 0 ),
+	gradient (     Model->getNparams(), 0 ),
+	currentgradient ( Model->getNparams(), 0 ),
+	stepsizes (    Model->getNparams(), .001),
+	Naccepted (0),
+	Nleapfrog(Nleap)
+{
+	proposal = new GaussRandom;
+
+	setTheta ( currenttheta );
+
+	stepsizes[0] = 0.001;
+	stepsizes[1] = 0.001;
+	stepsizes[2] = 0.0001;
+}
+
+std::vector<double> HybridMCMC::draw ( void ) {
+	int i;
+	const PsiPsychometric * model ( getModel() );
+	const PsiData *         data  ( getData() );
+
+	for (i=0; i<model->getNparams(); i++)
+		momentum[i] = proposal->draw();
+
+	currentH = 0;
+	for (i=0; i<model->getNparams(); i++)
+		currentH += momentum[i] * momentum[i];
+	currentH *= 0.5;
+	currentH += energy;
+
+	leapfrog();
+
+	newenergy = model->neglpost ( newtheta, data );
+	newH = 0;
+	for (i=0; i<model->getNparams(); i++)
+		newH += momentum[i] * momentum[i];
+	newH *= 0.5;
+	newH += newenergy;
+
+	if ( log(drand48()) < currentH-newH ) {
+		// Accept
+		for (i=0; i<model->getNparams(); i++) {
+			currenttheta[i] = newtheta[i];
+			currentgradient[i] = gradient[i];
+		}
+		energy = newenergy;
+		Naccepted ++;
+#ifdef DEBUG_MCMC
+		std::cerr << " * ";
+#endif
+	}
+#ifdef DEBUG_MCMC
+	else
+		std::cerr << "   ";
+	std::cout << currenttheta[0] << "\n";
+#endif
+	return currenttheta;
+}
+
+void HybridMCMC::setTheta ( const std::vector<double>& theta ) {
+	int i;
+	currenttheta = theta;
+
+	for (i=0; i<getModel()->getNparams(); i++) {
+		gradient[i] = getModel()->dlposteri ( currenttheta, getData(), i );
+	}
+	energy = getModel()->neglpost ( currenttheta, getData() );
+}
+
+void HybridMCMC::setstepsize ( const std::vector<double>& sizes ) {
+	if (sizes.size()==stepsizes.size())
+		stepsizes = sizes;
+	else
+		throw BadArgumentError();
+}
+
+void HybridMCMC::setstepsize ( double size, unsigned int param ) {
+	if ( param>=stepsizes.size() )
+		throw BadIndexError();
+
+	stepsizes[param] = size;
+}
+
+void HybridMCMC::leapfrog ( void ) {
+	int i,n;
+	int Nparams(getModel()->getNparams());
+	const PsiPsychometric * model (getModel());
+
+	gradient = currentgradient;
+	newtheta = currenttheta;
+
+	for (n=0; n<Nleapfrog; n++) {
+		for (i=0; i<Nparams; i++)
+			momentum[i] -= 0.5 * stepsizes[i] * gradient[i];
+
+		for (i=0; i<Nparams; i++)
+			newtheta[i] +=          stepsizes[i] * momentum[i];
+
+		for (i=0; i<Nparams; i++)
+			gradient[i] = model->dlposteri ( newtheta, getData(), i );
+
+		for (i=0; i<Nparams; i++)
+			momentum[i] -= 0.5 * stepsizes[i] * gradient[i];
+	}
+}
+
+double HybridMCMC::getDeviance ( void ) {
+	return getModel()->deviance ( currenttheta, getData() );
+}
+
+PsiMClist HybridMCMC::sample ( unsigned int N ) {
+	PsiMClist out ( N, getModel()->getNparams() );
+	int i;
+
+	for (i=0; i<N; i++) {
+		out.setEst ( i, draw(), 0. );
+		out.setdeviance ( i, getDeviance() );
+#ifdef DEBUG_MCMC
+		std::cerr
+			<< "H: "       << std::setiosflags(std::ios::fixed) << currentH
+			<< " H_: "     << std::setiosflags(std::ios::fixed) << newH
+			<< " E: "      << std::setiosflags(std::ios::fixed) << energy
+			<< " accept: " << std::setiosflags(std::ios::fixed) << double(Naccepted)/(i+1) << "\n";
+#endif
+	}
+
+#ifdef DEBUG_MCMC
+	std::cerr << "Acceptance rate: " << double(Naccepted)/N << "\n";
+#endif
+
+	return out;
 }
--- src/mcmc.h
+++ src/mcmc.h
@@ -4,25 +4,83 @@
 #include <vector>
 #include "psychometric.h"
 #include "rng.h"
+#include "mclist.h"
 
-class PsiMetropolisHastings
+class PsiSampler
 {
 	private:
-		const PsiPsychometric model;
-		const PsiData data;
-		const PsiRandom* propose;
+		const PsiPsychometric * model;
+		const PsiData * data;
+	public:
+		PsiSampler ( const PsiPsychometric * Model, const PsiData * Data ) : model(Model), data(Data) {}///< set up a sampler to sample from the posterior of the parameters of pmf given the data dat
+		virtual std::vector<double> draw ( void ) { throw NotImplementedError(); }                     ///< draw a sample from the posterior
+		virtual void setTheta ( const std::vector<double> theta ) { throw NotImplementedError(); }     ///< set the "state" of the underlying markov chain
+		virtual std::vector<double> getTheta ( void ) { throw NotImplementedError(); }                 ///< get the "state" of the underlying markov chain
+		virtual void setstepsize ( double size, unsigned int param ) { throw NotImplementedError(); }  ///< set the size of the steps for parameter param of the sampler
+		virtual void setstepsize ( const std::vector<double>& sizes ) { throw NotImplementedError(); } ///< set all stepsizes of the sampler
+		virtual double getDeviance ( void ) { throw NotImplementedError(); }                           ///< return the model deviance for the current state
+		virtual PsiMClist sample ( unsigned int N ) { throw NotImplementedError(); }                   ///< draw N samples from the posterior
+		const PsiPsychometric * getModel() const { return model; }                                     ///< return the underlying model instance
+		const PsiData         * getData()  const { return data;  }                                     ///< return the underlying data instance
+};
+
+class MetropolisHastings : public PsiSampler
+{
+	private:
+		PsiRandom* propose;
 		std::vector<double> currenttheta;
 		std::vector<double> newtheta;
 		std::vector<double> stepwidths;
 		double qold;
 		double currentdeviance;
+		int accept;
 	public:
-		// TODO: Should Sampler arguments be const? Should we store copies?
-		PsiMetropolisHastings ( const PsiPsychometric * pmf, const PsiData * dat );       ///< initialize the sampler
-		std::vector<double> sample ( void );                                              ///< perform a metropolis hastings step and draw a sample from the posterior
-		void setPrm ( std::vector<double>& prm );                                         ///< set the current state of the sampler
-		std::vector<double> getPrm ( void );                                              ///< get the current state of the sampler
+		MetropolisHastings (
+			const PsiPsychometric * Model,                                                  ///< psychometric funciton model to sample from
+			const PsiData * Data,                                                           ///< data to base inference on
+			PsiRandom* proposal                                                             ///< proposal distribution (will usually be a gaussian)
+			);                                                          ///< initialize the sampler
+		~MetropolisHastings ( void ) { delete propose; }
+		std::vector<double> draw ( void );                                                ///< perform a metropolis hastings step and draw a sample from the posterior
+		void setTheta ( const std::vector<double>& prm );                                 ///< set the current state of the sampler
+		std::vector<double> getTheta ( void ) { return currenttheta; }                    ///< get the current state of the sampler
+		double getDeviance ( void ) { return currentdeviance; }                           ///< get the current deviance
+		void setstepsize ( double size, unsigned int param );                             ///< set the standard deviation of the proposal distribution for parameter param
+		void setstepsize ( const std::vector<double>& sizes );                            ///< set standard deviations of the proposal distribution for all parameters at once
+		PsiMClist sample ( unsigned int N );                                              ///< draw N samples from the posterior
+};
+
+class HybridMCMC : public PsiSampler
+{
+	private:
+		PsiRandom* proposal;
+		std::vector<double> currenttheta;
+		std::vector<double> newtheta;
+		std::vector<double> momentum;
+		double currentH;
+		double newH;
+		double energy;
+		double newenergy;
+		std::vector<double> gradient;
+		std::vector<double> currentgradient;
+		std::vector<double> stepsizes;
+		int Nleapfrog;
+		int Naccepted;
+		void leapfrog ( void );
+	public:
+		HybridMCMC (
+			const PsiPsychometric * Model,                                                  ///< psychometric function model to sample from
+			const PsiData * Data,                                                          ///< data to base inference on
+			int Nleap                                                                     ///< number of leapfrog steps to be performed for each sample
+			);                                                             ///< initialize the sampler
+		~HybridMCMC ( void ) { delete proposal; }
+		std::vector<double> draw ( void );                                                ///< draw a sample from the posterior
+		void setTheta ( const std::vector<double>& prm );                                 ///< set the current state of the sampler
+		std::vector<double> getTheta ( void ) { return currenttheta; }                    ///< get the current state of the sampler
+		void setstepsize ( double size, unsigned int param );                             ///< set stepsize of the leapfrog integration for parameter param
+		void setstepsize ( const std::vector<double>& sizes );                            ///< set all stepsizes of leapfrog integration for all parameters at once
 		double getDeviance ( void );                                                      ///< get the current deviance
+		PsiMClist sample ( unsigned int N );                                              ///< draw N samples from the posterior
 };
 
 #endif
