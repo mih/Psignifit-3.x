5a7297c4f10069264344cacb104245ff902b199f
Ingo Fründ <ingo.fruend@googlemail.com>
2009-09-09 17:43:52 +0200

    [RF] a common header that includes all files

---
+++ src/psipp.h
@@ -0,0 +1,17 @@
+#ifndef PSIPP_H
+#define PSIPP_H
+
+#include "bootstrap.h"
+#include "core.h"
+#include "data.h"
+#include "errors.h"
+#include "mclist.h"
+#include "mcmc.h"
+#include "optimizer.h"
+#include "prior.h"
+#include "psychometric.h"
+#include "rng.h"
+#include "sigmoid.h"
+#include "special.h"
+
+#endif
