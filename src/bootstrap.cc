a8de3f3e31cc97539c6a5ca6571411ee160e1775
Ingo Fründ <ingo.fruend@googlemail.com>
2009-09-21 16:42:53 +0200

    [RF] parametric bootstrap splitted

--- src/bootstrap.cc
+++ src/bootstrap.cc
@@ -5,6 +5,51 @@
 #include <iostream>
 #endif
 
+void newsample ( const PsiData * data, const std::vector<double>& p, std::vector<int> * sample ) {
+	/* Draw a new sample from the psychometric function */
+	BinomialRandom binomial ( 10, 0.5 );    // Initialize with nonsense parameters
+	int k;                                            // Block index
+
+	for ( k=0; k<data->getNblocks(); k++ ) {
+		binomial.setprm ( data->getNtrials(k), p[k] );
+		(*sample)[k] = binomial.draw ();
+	}
+}
+
+void determineBCa ( const std::vector<double>& l_LF, const std::vector<double>& u_i, double initialthreshold, double *bias, double*acc ) {
+	// Calculate BCa constants
+	double E_l(0);     // expected likelihood
+	double E_l3(0);    // expectation of cubic likelihood
+	double var_l(0);   // variance / standard deviation of likelihood
+	double w(0);       // probability to obtain thresholds less than ML-fit
+
+	int b,B ( l_LF.size() );
+
+	// Get expectations
+	for ( b=0; b<B; b++ ) {
+		E_l += l_LF[b];
+		E_l3 += l_LF[b]*l_LF[b]*l_LF[b];
+		w += int( u_i[b] < initialthreshold );
+	}
+
+	// Get variance/standard deviation
+	for ( b=0; b<B; b++ ) {
+		var_l += (l_LF[b]-E_l)*(l_LF[b]-E_l);
+	}
+	var_l /= B-1;
+	var_l = sqrt(var_l); // note that this is not variance anymore but standard deviation
+
+#ifdef DEBUG_BOOTSTRAP
+	std::cerr << " E_l3=" << E_l3 << "  var_l="<< var_l << "\n";
+	std::cerr << " w = " << w/(B+1) << " real_w = " << invPhi(w/(B+1)) << "\n";
+#endif
+
+	// output
+	*bias = invPhi(w/(B+1));
+	*acc  = E_l3 / (6*var_l*var_l*var_l);
+}
+
+
 
 BootstrapList parametricbootstrap ( int B, const PsiData * data, const PsiPsychometric* model, std::vector<double> cuts, std::vector<double>* param, bool BCa )
 {
@@ -12,39 +57,37 @@ BootstrapList parametricbootstrap ( int B, const PsiData * data, const PsiPsycho
 	std::cerr << "Starting bootstrap\n Cuts size=" << cuts.size() << " "; std::cerr.flush();
 #endif
 	BootstrapList bootstrapsamples ( B, model->getNparams(), data->getNblocks(), cuts );
-	int b,k,l,cut;
-	std::vector< std::vector<double> > l_LF (B, std::vector<double>(cuts.size()));
-	std::vector< std::vector<double> > u_i (B, std::vector<double>(cuts.size()));
-	PsiOptimizer opt ( model, data );
-	PsiData * localdataset = new PsiData ( data->getIntensities(),
+	int b,k,l,cut;                               // iteration variables for bootstrap sample, block, l-general purpose third level iteration, cut
+	std::vector< std::vector<double> > l_LF (cuts.size(), std::vector<double>(B));   // vector of double-vectors
+	std::vector< std::vector<double> > u_i  (cuts.size(), std::vector<double>(B));
+	PsiOptimizer opt ( model, data );                          // for ML-Fitting
+	PsiData * localdataset = new PsiData ( data->getIntensities(),  // local because it changes in every iteration
 			data->getNtrials(),
 			data->getNcorrect(),
 			data->getNalternatives() );
 
-	std::vector<double> initialfit ( model->getNparams() );
+	std::vector<double> initialfit ( model->getNparams() );       // generating parameters for the bootstrap samples
 	if (param==NULL)
 		initialfit = opt.optimize( model, data );
 	else
 		initialfit = *param;
+	std::vector<double> p          ( data->getNblocks() );       // predicted p-correct for parametric bootstrap
+	for ( k=0; k<data->getNblocks(); k++ ) { p[k] = model->evaluate ( data->getIntensity(k), initialfit ); }
+
 	std::vector<double> localfit   ( model->getNparams() );
 	std::vector<int>    sample     ( data->getNblocks() );
 	std::vector<double> initialthresholds ( cuts.size() );
 	std::vector<double> devianceresiduals ( data->getNblocks() );
-	double p,deviance;
-	BinomialRandom binomial ( 10, 0.5 );
+	double deviance;
 
 	for (cut=0; cut<cuts.size(); cut++)
 		initialthresholds[cut] = model->getThres(initialfit,cuts[cut]);
 
 	for ( b=0; b<B; b++ ) {
 		// Resampling
-		for ( k=0; k<data->getNblocks(); k++ ) {
-			p = model->evaluate( data->getIntensity(k), initialfit );
-			binomial.setprm ( data->getNtrials(k), p );
-			sample[k] = binomial.draw ();
-		}
-		localdataset->setNcorrect ( sample );
-		bootstrapsamples.setData ( b, sample );
+		newsample ( data, p, &sample );         // draw a new sample
+		localdataset->setNcorrect ( sample );   // put the new sample to the localdataset
+		bootstrapsamples.setData ( b, sample ); // store the new sample in the mc object
 
 		// Fit
 		localfit = opt.optimize (model, localdataset );
@@ -52,6 +95,8 @@ BootstrapList parametricbootstrap ( int B, const PsiData * data, const PsiPsycho
 		if (false)
 		std::cerr << localfit[0] << " " << localfit[1] << " " << localfit[2] << "\n";
 #endif
+
+		// Get some characteristics of the localfit
 		deviance = model->deviance ( localfit, localdataset );
 		devianceresiduals = model->getDevianceResiduals ( localfit, localdataset );
 		bootstrapsamples.setEst ( b, localfit, deviance );
@@ -60,19 +105,21 @@ BootstrapList parametricbootstrap ( int B, const PsiData * data, const PsiPsycho
 
 		// Store what we need for the BCa stuff
 		for (cut=0; cut<cuts.size(); cut++) {
-			l_LF[b][cut] = model->leastfavourable ( localfit, localdataset, cuts[cut] );
+			l_LF[cut][b] = model->leastfavourable ( localfit, localdataset, cuts[cut] );
 #ifdef DEBUG_BOOTSTRAP
-			if (l_LF[b][cut] != l_LF[b][cut])
-			std::cerr << "l_LF["<<b<<"]["<<cut<<"] = " << l_LF[b][cut] << "\n";
+			if (l_LF[cut][b] != l_LF[cut][b]) {
+				std::cerr << "l_LF["<<cut<<"]["<<b<<"] = " << l_LF[cut][b] << "\n";
+			}
 #endif
-			u_i[b][cut]  = model->getThres(localfit,cuts[cut]);
+			u_i[cut][b]  = model->getThres(localfit,cuts[cut]);
 #ifdef DEBUG_BOOTSTRAP
-			if (l_LF[b][cut]!= l_LF[b][cut])
-			std::cerr << "u_i["<<b<<"]["<<cut<<"] = " << u_i[b][cut] << "\n";
+			if (l_LF[cut][b]!= l_LF[cut][b]) {
+				std::cerr << "u_i["<<cut<<"]["<<b<<"] = " << u_i[cut][b] << "\n";
+			}
 #endif
-			bootstrapsamples.setThres(u_i[b][cut], b, cut);
+			bootstrapsamples.setThres(u_i[cut][b], b, cut);
 
-			if (l_LF[b][cut] != l_LF[b][cut]) {
+			if (l_LF[cut][b] != l_LF[cut][b]) {
 				// TODO: if l_LF is nan we don't take this sample
 				// TODO: This is not the best solution but it works
 				b--;
@@ -82,28 +129,10 @@ BootstrapList parametricbootstrap ( int B, const PsiData * data, const PsiPsycho
 	}
 
 	// Calculate BCa constants
-	double E_l,E_l3, var_l,w;
+	double bias, acc;
 	for (cut=0; cut<cuts.size(); cut++) {
-		// Get expectations
-		E_l = 0; E_l3 = 0; w=0;
-		for ( b=0; b<B; b++ ) {
-			E_l += l_LF[b][cut];
-			E_l3 += l_LF[b][cut]*l_LF[b][cut]*l_LF[b][cut];
-			w += int( u_i[b][cut] < initialthresholds[cut] );
-		}
-		var_l = 0;
-		for ( b=0; b<B; b++ ) {
-			var_l += (l_LF[b][cut]-E_l)*(l_LF[b][cut]-E_l);
-		}
-		var_l /= B-1;
-		var_l = sqrt(var_l); // note that this is not variance anymore but standard deviation
-
-		// Store BCa stuff
-#ifdef DEBUG_BOOTSTRAP
-	std::cerr << " E_l3=" << E_l3 << "  var_l="<< var_l << "\n";
-	std::cerr << " w = " << w/(B+1) << " real_w = " << invPhi(w/(B+1)) << "\n";
-#endif
-		bootstrapsamples.setBCa(cut, invPhi(w/(B+1)), E_l3/(6*var_l*var_l*var_l));
+		determineBCa ( l_LF[cut], u_i[cut], initialthresholds[cut], &bias, &acc );
+		bootstrapsamples.setBCa(cut, bias, acc );
 	}
 
 	delete localdataset;
