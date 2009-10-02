cdf1d258b36aabd01554202c037e1d6cc35a1e1a
Ingo Fründ <ingo.fruend@googlemail.com>
2009-10-02 18:34:24 +0200

    [NF] Observer counts responses internally, can do whole experiments

--- src/psigobservers.py
+++ src/psigobservers.py
@@ -47,11 +47,19 @@ class Observer ( object ):
         else:
             self.a,self.b,self.lapse = params
             self.guess = 1./model["nafc"]
+
+        # Make sure, a,b,lapse and guess are floats
+        self.a = float(self.a)
+        self.b = float(self.b)
+        self.lapse = float(self.lapse)
+        self.guess = float(self.guess)
+
         self.model = {
                 "sigmoid": model.setdefault ( "sigmoid", "logistic" ),
                 "core":    model.setdefault ( "core",    "ab" ),
                 "nafc":    model.setdefault ( "nafc",    2 )
                 }
+        self.data = []
 
     def DoATrial ( self, stimulus_intensity=1 ):
         """Simulate a single trial
@@ -63,7 +71,16 @@ class Observer ( object ):
         prob = float( _psipy.diagnostics ( [stimulus_intensity], self.params,
             sigmoid=self.model["sigmoid"], core=self.model["core"], nafc=self.model["nafc"] ) )
 
-        return int ( N.random.rand()<prob )
+        resp = int ( N.random.rand()<prob )
+
+        if len(self.data) == 0 or not stimulus_intensity==self.data[-1][0]:
+            self.data.append ( [stimulus_intensity,resp,1] )
+        else:
+            self.data[-1][1] += resp
+            self.data[-1][2] += 1
+
+        return resp
+
 
     def DoABlock ( self, stimulus_intensity=1, ntrials=50 ):
         """Simulate a block of trials
@@ -73,11 +90,47 @@ class Observer ( object ):
                 stimulus intensity to be presented
             *ntrials* :
                 number of trials in the block
+
+        :Output:
+            The number of Yes-responses (in Yes/No) or the number of correct responses (nAFC)
         """
         prob = float( _psipy.diagnostics ( [stimulus_intensity], self.params,
             sigmoid=self.model["sigmoid"], core=self.model["core"], nafc=self.model["nafc"] ) )
-        return N.random.binomial ( ntrials, prob )
 
+        resp = N.random.binomial ( ntrials, prob )
+
+        self.data.append ( [stimulus_intensity, resp, ntrials] )
+
+        return resp
+
+    def DoAnExperiment ( self, stimulus_intensities, ntrials=50 ):
+        """Simulate a whole experiment
+
+        :Parameters:
+            *stimulus_intensities* :
+                a sequence of stimulus intensities in the order they should be presented
+            *ntrials* :
+                Either an integer or a sequence. If this is an integer, it is interpreted as a constant
+                number of trials that is presented in each trial, otherwise, the sequence is expected to
+                contain a number of trials for each trials.
+
+        :Output:
+            A list of lists. Each element of the list has the structure
+            [stimulus_intensity, number_of_correct_or_yes_responses,number_of_trials]
+        """
+        if isinstance ( ntrials, int ):
+            ntrials = [ntrials] * len(stimulus_intensities)
+
+        data = []
+
+        for s,n in zip ( stimulus_intensities, ntrials ):
+            data.append ( [s, self.DoABlock ( s, n ), n] )
+
+        return data
+
+    def seed ( self, seed ):
+        """Seed the underlying random number generator to a defined value"""
+        N.random.seed ( seed )
 
     @Property
     def params ():
