7ba58c6ab3e69bc1eaa95d97dbc710e61a3f1e69
Valentin Haenel <valentin.haenel@gmx.de>
2010-07-02 09:59:53 +0200

    [NF] add fixed levels, and proper 'w' to condor submission script

--- scripts/bootstrap_vs_bayes/write_submission_condor.py
+++ scripts/bootstrap_vs_bayes/write_submission_condor.py
@@ -25,28 +25,52 @@ error_suffix = ".error"
 
 
 if os.path.exists(sim_root):
-    raise Exception("simulation data directory exists, DON'T OVERWRITE IT!!!")
+    #raise Exception("simulation data directory exists, DON'T OVERWRITE IT!!!")
+    pass
 else:
     os.makedirs (data_root)
     os.makedirs (log_root)
 
+# create levels
+
+levels = [[ 3.        ,  3.80913488,  4.3712636 ,  6.09132917],
+          [ 2.        ,  2.88539834,  3.45362578,  3.93235111,  4.4016988 ,
+            4.931913  ,  5.67413566,  8.18265834],
+          [ 2.        ,  2.48609305,  2.8408528 ,  3.13126402,  3.38540599,
+            3.61826975,  3.83938243,  4.05584552,  4.27390645,  4.50009965,
+            4.7425272 ,  5.01299204,  5.33173313,  5.74110516,  6.36386887,
+            8.18265834],
+          [ 2.        ,  2.259244  ,  2.47302265,  2.65706792,  2.8203285 ,
+            2.96840501,  3.10505633,  3.23294705,  3.35405383,  3.46990263,
+            3.58171565,  3.69050733,  3.79715025,  3.90242298,  4.00704705,
+            4.11171774,  4.21713221,  4.32401778,  4.43316336,  4.54545756,
+            4.66193847,  4.78386279,  4.91280722,  5.05082464,  5.20069835,
+            5.36638121,  5.55381231,  5.77258678,  6.03984177,  6.39121983,
+            6.92385846,  8.18265834]]
+
 # Simulations
+
+def str_no_space(l):
+    """ concatenate list of numbers into string without spaces """
+    return "["+",".join([str(i) for i in l])+"]"
 
 job_number = 0
 
-for (nblocks, blocksize, lapse_rate, width, nalt, gen_sigmoid, ana_sigmoid) in \
-    ((nblocks, blocksize, lapse_rate, width, nalt, gen_sigmoid, ana_sigmoid)
-            for nblocks     in map(str, [4, 8, 16, 32])
+for (levels, blocksize, lapse_rate, width, nalt, gen_sigmoid, ana_sigmoid) in \
+    ((levels, blocksize, lapse_rate, width, nalt, gen_sigmoid, ana_sigmoid)
+            for levels     in map(str_no_space, levels)
             for blocksize   in map(str, [10, 20, 40])
             for lapse_rate  in map(str, [0.01, 0.05])
-            for width       in map(str, [0.1, 0.2, 0.3])
+            for width       in map(str, [0.5, 2.0, 4])
             for nalt        in map(str, [1, 2])
             for gen_sigmoid in ["logistic", "gumbel_r"]
             for ana_sigmoid in ["logistic", "gumbel_r"]):
+    nblocks = len(eval(levels))
     output += "#job id = %d\n" % (job_number)
     output += "output = " + os.path.join(log_root, "job"+ str(job_number) + log_suffix) + "\n"
     output += "error = "  + os.path.join(log_root, "job"+ str(job_number) + error_suffix) + "\n"
-    output += "arguments = --nblocks=" + (nblocks)+\
+    output += "arguments = --nblocks=" + str(nblocks)+\
+                         " --fixed-levels=" + levels+\
                          " --blocksize=" + (blocksize)+\
                          " --gen-prm3=" + lapse_rate +\
                          " --gen-prm2=" + width +\
