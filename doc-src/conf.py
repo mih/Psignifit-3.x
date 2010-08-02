05b5a3cb6e59cc895e6ea88f32dd924d0401a609
Ingo Fründ <ingo.fruend@googlemail.com>
2010-08-02 16:27:32 +0200

    [DOC] added sphinx matplotlib extensions to conf.py for documentation

--- doc-src/conf.py
+++ doc-src/conf.py
@@ -26,8 +26,14 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
-'sphinx.ext.autosummary', 'extapi']
+extensions = [ 'sphinx.ext.autodoc',
+            'sphinx.ext.doctest',
+            'sphinx.ext.autosummary',
+            'extapi',
+            'matplotlib.sphinxext.mathmpl',
+            'matplotlib.sphinxext.only_directives',
+            'matplotlib.sphinxext.plot_directive',
+            ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
