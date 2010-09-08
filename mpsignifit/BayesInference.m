56e8b88fc7b39926268628f6363f2106d2af59a3
Ingo Fründ <ingo.fruend@googlemail.com>
2010-09-08 18:14:41 +0200

    matlab[DOC] documentation for BayesInference

--- mpsignifit/BayesInference.m
+++ mpsignifit/BayesInference.m
@@ -1,5 +1,101 @@
 function results = BayesInference ( data, priors, varargin )
-% results = BayesInference ( data, priors, sample=2000, cuts=[0.25, 0.5, 0.75], core='mw0.1', sigmoid='logistic', nafc=2
+% results = BayesInference ( data, priors, ... )
+%
+% Bayesian inference for psychometric functions using markov chain monte carlo.
+%
+% data should be an array with three columns and one row for each block of trials.
+%    The first column should contain the stimulus intensity in the respective block,
+%    the second column should contain the number of correctly identified trials in
+%    the respective block, and the third column should contain the total number of
+%    trials presented in the respective block.
+%
+% priors should be a struct with the fields m_or_a, w_or_b, lambda, and gamma. 'None'
+%    can be used to specify "No prior" i.e. an improper, flat prior. A valid prior would
+%    be
+%
+%    >> priors.m_or_a = 'None';
+%    >> priors.w_or_b = 'None';
+%    >> priors.lambda = 'Uniform(0,.1)';
+%    >> prior.gamma   = 'Uniform(0,.1)';
+%
+%    For more information on the specification of priors for psychometric functions, see
+%
+%    http://psignifit.sourceforge.net/BAYESINTRO.html#specification-of-prior-distributions
+%
+% In the default configuration, the function draws samples from the posterior distribution
+% of model parameters given the data using the Metropolis-Hastings algorithm. The following
+% parameters can be used to modify the behavior of the function. To specify one of these
+% parameters, call the function as
+%
+% >> BayesInference ( data, priors, 'PARAM', VALUE )
+%
+% Some parameters don't require a value (for instance the 'gammaislambda' setting). In this
+% case call the function as
+%
+% >> BayesInference ( data, priors, 'PARAM' )
+%
+% In the folloing list, all valid parameters are given. If a parameter requires a value argument,
+% the type of this value argument is given after the name of the parameter and separated by a comma.
+%
+% Parameters
+% ----------
+%
+% 'nafc', integer               Default: 2
+%       number of alternatives in the analyzed task. If nafc is 1 this means that a yes-no task was used.
+%       In this case, the lower asymptote is considered a free parameter and is fitted as a fourth parameter.
+%       See gammaislambda below too.
+%
+% 'sigmoid', char               Default: 'logistic'
+%       Name of the sigmoid object to be used for fitting. psignifit includes a large number of sigmoids.
+%       These include (but are not restricted to) 'logistic', 'gauss', or 'gumbel'. See
+%
+%       http://psignifit.sourceforge.net/MODELSPECIFICATION.html#specifiing-the-shape-of-the-psychometric-function
+%
+%       for further information.
+%
+% 'core', char                  Default: 'mw0.1'
+%       Name of the core object to be used for fitting. psignifit includes a large number of cores. These
+%       include (but are not restricted to) 'ab', 'mw0.1'. See
+%
+%       http://psignifit.sourceforge.net/MODELSPECIFICATION.html#specifiing-the-shape-of-the-psychometric-function
+%
+%       for further information.
+%
+% 'gammaislambda'               Default: false
+%       In yes-no tasks, it is sometimes of interest to constrain the model to have the same upper and lower
+%       asymptote. This can be achieved in psignifit by constraining gamma to be lambda, effectively reducing the
+%       number of free parameters by one.
+%
+% 'verbose'                     Default: false
+%       print out more about what is going on.
+%
+% 'cuts', double (vector)       Default: [0.25, 0.5, 0.75]
+%       Cuts are the performances at which thresholds should be determined. Thus, setting cuts to 0.5 will define
+%       the threshold to be the signal level that corresponds to a performance half way between the upper and the
+%       lower asymptote.
+%
+% 'samples', int                Default: 2000
+%       Number of MCMC samples to be generated
+%
+% 'pilot', double array         Default: false
+%       An array of pilot samples to tune the markov chain monte carlo procedure. A good idea is often to use
+%       a small number of bootstrap samples to tune the sampler, such as
+%
+%       >> pilot = BootstrapInference ( data, priors, 'samples', 200 );
+%       >> mcmc  = BayesInference ( data, priors, 'pilot', pilot.mcestimates );
+%
+%       By default, NO automatic tuning is performed!
+%
+% 'stepwidths', double vector   Default: [0.1, 0.1, 0.01]
+%       standard deviations of the different components of the proposal distribution. Also see 'pilot'
+%
+% 'generic'                     Default: false
+%       Use the generic Metropolis Hastings algorithm proposed by Raftery and Lewis. This is often a good choice if you
+%       use a pilot sample.
+%
+%
+% This file is part of psignifit3 for matlab. (c) 2010 by Ingo Fründ
+
 psignifitpath = '../cli';
 
 % Check data format
