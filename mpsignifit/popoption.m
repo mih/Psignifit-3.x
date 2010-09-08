bd5822a7dbdeaeef2dd01909222e4b766c6246b8
Ingo Fründ <ingo.fruend@googlemail.com>
2010-09-08 18:26:56 +0200

    matlab[DOC] documenation for popoption

--- mpsignifit/popoption.m
+++ mpsignifit/popoption.m
@@ -1,5 +1,8 @@
 function [value,args] = popoption ( args )
 % pop the first value of args and return args
+%
+%
+% This file is part of psignifit3 for matlab (c) by Ingo Fründ
 
 value = args{1};
 if length(args)>1
