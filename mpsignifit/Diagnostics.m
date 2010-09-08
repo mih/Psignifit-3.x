6595d0d44a7c5e3d702f0114cffea09e22663279
Ingo Fründ <ingo.fruend@googlemail.com>
2010-09-08 18:21:31 +0200

    matlab[DOC] documentation for Diagnostics.m

--- mpsignifit/Diagnostics.m
+++ mpsignifit/Diagnostics.m
@@ -1,10 +1,65 @@
 function results = Diagnostics ( data, parameters, varargin )
 % results = Diagnostics ( data, parameters, ... )
 %
-% Determine some parameters of fitted psychometric function parameters
-% See BootstrapInference for more information about additional parameters
+% Determine some parameters of fitted psychometric function parameters.
+% This involves evaluation of the function for different x values or determining
+% some diagnostic values for goodness of fit.
 %
-% This file is 
+% data should be an array with three columns and one row for each block of trials.
+%    The first column should contain the stimulus intensity in the respective block,
+%    the second column should contain the number of correctly identified trials in
+%    the respective block, and the third column should contain the total number of
+%    trials presented in the respective block.
+%
+% parameters should a vector of parameters at which the diagnostic values should
+%    be evaluated.
+%
+% The bevavior of this function can be modified by specifiing certain addition
+% parameters. To specify one of these parameters, call the function as
+%
+% >> Diagnostics ( data, priors, 'PARAM', VALUE )
+%
+% Some parameters don't require a value (for instance the 'gammaislambda' setting). In this
+% case call the function as
+%
+% >> Diagnostics ( data, priors, 'PARAM' )
+%
+% In the folloing list, all valid parameters are given. If a parameter requires a value argument,
+% the type of this value argument is given after the name of the parameter and separated by a comma.
+%
+% Parameters
+% ----------
+%
+% 'nafc', integer               Default: 2
+%       number of alternatives in the analyzed task. If nafc is 1 this means that a yes-no task was used.
+%       In this case, the lower asymptote is considered a free parameter and is fitted as a fourth parameter.
+%       See gammaislambda below too.
+%
+% 'sigmoid', char               Default: 'logistic'
+%       Name of the sigmoid object to be used for fitting. psignifit includes a large number of sigmoids.
+%       These include (but are not restricted to) 'logistic', 'gauss', or 'gumbel'. See
+%
+%       http://psignifit.sourceforge.net/MODELSPECIFICATION.html#specifiing-the-shape-of-the-psychometric-function
+%
+%       for further information.
+%
+% 'core', char                  Default: 'mw0.1'
+%       Name of the core object to be used for fitting. psignifit includes a large number of cores. These
+%       include (but are not restricted to) 'ab', 'mw0.1'. See
+%
+%       http://psignifit.sourceforge.net/MODELSPECIFICATION.html#specifiing-the-shape-of-the-psychometric-function
+%
+%       for further information.
+%
+% 'gammaislambda'               Default: false
+%       In yes-no tasks, it is sometimes of interest to constrain the model to have the same upper and lower
+%       asymptote. This can be achieved in psignifit by constraining gamma to be lambda, effectively reducing the
+%       number of free parameters by one.
+%
+% 'verbose'                     Default: false
+%       print out more about what is going on.
+%
+% This function is part of psignifit3 for matlab (c) 2010 by Ingo Fründ
 
 psignifitpath = '../cli';
 
