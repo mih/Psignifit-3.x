35f0d3e7ec6996249ea84386c3304981af40563d
Ingo Fründ <ingo.fruend@googlemail.com>
2010-09-09 17:30:14 +0200

    C++[RF] avoiding drand48, using L'Ecuyer rng like in Numerical Recipes

--- src/rng.cc
+++ src/rng.cc
@@ -4,6 +4,49 @@
  */
 #include "rng.h"
 
+long __psignifit_rng_idum (-1);
+
+double PsiRandom::rngcall ( void ) {
+	// Long period (>2*10e18) random number generator of L'Ecuyer with Bays-Durham shuffle
+	// and added safeguards. Returns a uniform random deviate between 0.0 and 1.0 (exclusive
+	// of the endpoints). Uses __psignifit_rng_idum as seed which should be negative and not
+	// be changed during a simulation run.
+	// RNMX should approximate the largest floating point value that is less than 1.
+	//
+	// This has been copied from numerical recipes
+	long *idum = &__psignifit_rng_idum;
+	int j;
+	long k;
+	static long idum2 ( 123456789 );
+	static long iy ( 0 );
+	static long iv[NTAB];
+	double temp;
+
+	if ( *idum <= 0 ) {
+		if ( -(*idum) < 1 ) *idum=1;
+		else *idum = (-*idum);
+		idum2 = (*idum);
+		for ( j = NTAB+7; j>=0; j-- ) {
+			k = (*idum) / IQ1;
+			*idum = IA1*(*idum-k*IQ1) - k*IR1;
+			if ( *idum<0 ) *idum += IM1;
+			if ( j<NTAB ) iv[j] = *idum;
+		}
+		iy = iv[0];
+	}
+	k = (*idum)/IQ1;
+	*idum = IA1*(*idum-k*IQ1) - k*IR1;
+	if ( *idum<0 ) *idum += IM1;
+	k = idum2 / IQ2;
+	idum2 = IA2*(idum2-k*IQ2) - k*IR2;
+	j = iy/NDIV;
+	iy = iv[j] - idum2;
+	iv[j] = *idum;
+	if ( iy<1 ) iy += IMM1;
+	if ( (temp=AM*iy)>RNMX ) return RNMX;
+	else return temp;
+}
+
 double GaussRandom::draw ( void )
 {
 	if ( good ) {
@@ -33,5 +76,11 @@ double BinomialRandom::draw ( void )
 }
 
 void setSeed(long int seedval){
-    srand48(seedval);
+	if ( seedval==0 ) {
+		__psignifit_rng_idum = -1;
+	} else if ( seedval > 0 ) {
+		__psignifit_rng_idum = -seedval;
+	} else {
+		__psignifit_rng_idum = seedval;
+	}
 }
