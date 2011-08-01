6ac5c275b2544f7b2b00d2f3d7fa72fc76c38107
Valentin Haenel <valentin.haenel@gmx.de>
2011-08-01 15:37:48 +0200

    BF/UT: the b_range function no longer scales b
    This was changed in:
    
        5644d08d62585b0ff354cbdb939e6883124deb02
    
    Since it is much simpler now, we adapt the test
    values.

--- src/tests_all.cc
+++ src/tests_all.cc
@@ -1210,10 +1210,10 @@ int GetstartTest ( TestSuite * T ) {
 	failures += T->isequal ( ymax, 10, "2afc: maximum of alpha range", 1e-5 );
 	b_range ( data, &xmin, &xmax );
 	parameter_range ( data, pmf, 1, &ymin, &ymax );
-	failures += T->isequal ( xmin, 0.45512, "2afc: minimum of beta range", 1e-5 );
-	failures += T->isequal ( xmax, 2.2756, "2afc: maximum of beta range", 1e-5 );
-	failures += T->isequal ( ymin, 0.45512, "2afc: minimum of beta range", 1e-5 );
-	failures += T->isequal ( ymax, 2.2756, "2afc: maximum of beta range", 1e-5 );
+	failures += T->isequal ( xmin, 2.0, "2afc: minimum of beta range", 1e-5 );
+	failures += T->isequal ( xmax, 10.0, "2afc: maximum of beta range", 1e-5 );
+	failures += T->isequal ( ymin, 2.0, "2afc: minimum of beta range", 1e-5 );
+	failures += T->isequal ( ymax, 10.0, "2afc: maximum of beta range", 1e-5 );
 	lm_range ( data, &xmin, &xmax );
 	parameter_range ( data, pmf, 2, &ymin, &ymax );
 	failures += T->isequal ( xmin,  0, "2afc: minimum of lambda range", 1e-5 );
