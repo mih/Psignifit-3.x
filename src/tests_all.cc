9845709008e0b555856fb98899b66f468fa49131
Valentin Haenel <valentin.haenel@gmx.de>
2011-08-17 15:28:09 +0200

    RF: correct values for Bias UT

--- src/tests_all.cc
+++ src/tests_all.cc
@@ -520,12 +520,12 @@ int BootstrapTest ( TestSuite * T ) {
 	// Check against psignifit results
 	// These values are subject to statistical variation. "equality" is defined relatively coarse
 	failures += T->isless(boots.getAcc_t(0),     0.018662,"Acceleration constant (threshold)");
-	failures += T->isequal(boots.getBias_t(0),  -0.0677307,"Bias (threshold)",            .01);
+	failures += T->isequal(boots.getBias_t(0), -0.0928786,"Bias (threshold)",            .01);
 	failures += T->isequal(boots.getThres(.1,0), 2.65266,"th(.1)",                        .05);
 	failures += T->isequal(boots.getThres(.9,0), 3.89757,"th(.9)",                        .05);
 
 	failures += T->isequal(boots.getAcc_s(0),    -0.000155314, "Acceleration constant (slope)", .01);
-	failures += T->isequal(boots.getBias_s(0),    -0.0325919,   "Bias (slope)",                  .01);
+	failures += T->isequal(boots.getBias_s(0),    -0.0501536,   "Bias (slope)",                  .01);
 	failures += T->isequal(boots.getSlope(0.1,0), 0.181289,    "sl(.1)",                        .01);
 	failures += T->isequal(boots.getSlope(0.9,0), 0.497512,    "sl(.9)",                        .01);
 
