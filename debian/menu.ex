106714fb8dc5440e04c2b8d2b008d31ab47d4b4b
Michael Hanke <michael.hanke@gmail.com>
2011-08-28 11:38:25 +0200

    DEBIAN: Remove packaging stuff we don't need.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(psignifit3):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="psignifit3" command="/usr/bin/psignifit3"
--- debian/psignifit3.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the psignifit3 package
-#
-0 4	* * *	root	[ -x /usr/bin/psignifit3_maintenance ] && /usr/bin/psignifit3_maintenance
